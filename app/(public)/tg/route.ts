464419086777e64729f541d4751962a947c4cca1
Sourov Biswas <sourovcodes@gmail.com>
2025-09-06 22:59:10 +0600

    delete all

--- app/(public)/tg/route.ts
+++
@@ -1,5 +0,0 @@
-import { redirect } from 'next/navigation'
-
-export async function GET() {
-    redirect('https://t.me/+X94KLytY-Kk5NzU9')
-}
--- migrations/0001_warm_hulk.sql
+++
@@ -1,2 +0,0 @@
-CREATE UNIQUE INDEX "emailUniqueIndex" ON "user" USING btree (lower("email"));--> statement-breakpoint
-CREATE UNIQUE INDEX "usernameUniqueIndex" ON "user" USING btree (lower("username"));
