bca2a7f93267e46d17654b674994d72f01a2effb
Sourov Biswas <sourovcodes@gmail.com>
2025-09-10 14:20:37 +0600

    refactor: remove Settings controllers, routes, and related views to streamline the codebase

--- routes/web.php
+++ routes/web.php
@@ -23,11 +23,5 @@ Route::get('/terms-and-conditions', function () {
     return Inertia::render('terms-and-conditions');
 })->name('terms-and-conditions');
 
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('dashboard', function () {
-        return Inertia::render('dashboard');
-    })->name('dashboard');
-});
 
-require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
