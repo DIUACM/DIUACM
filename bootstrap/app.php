aefc4ea6f4ccb5e459d1010fced23e16ade17d62
Sourov Biswas <sourovcodes@gmail.com>
2025-09-26 07:42:33 +0600

    chore: comment out user redirection middleware in app configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -25,8 +25,8 @@ return Application::configure(basePath: dirname(__DIR__))
             AddLinkHeadersForPreloadedAssets::class,
         ]);
 
-        $middleware->redirectUsersTo('/');
-        $middleware->redirectGuestsTo('/admin/login');
+        // $middleware->redirectUsersTo('/');
+        // $middleware->redirectGuestsTo('/admin/login');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         $exceptions->respond(function (Response $response, Throwable $exception, Request $request) {
