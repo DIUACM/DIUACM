bde6f7654e394a61ef2398676d3c8767202cdfee
Sourov Biswas <sourovcodes@gmail.com>
2025-11-20 00:45:56 +0600

    feat: implement Payment resource with create, edit, list functionalities and associated schemas

---
+++ app/Filament/ContestAdmin/Resources/Payments/Schemas/PaymentForm.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Filament\ContestAdmin\Resources\Payments\Schemas;
+
+use Filament\Schemas\Schema;
+
+class PaymentForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                //
+            ]);
+    }
+}
