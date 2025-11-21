01cc40b0c0efb7c7c7d9a7eb3683552aeb055398
Sourov Biswas <sourovcodes@gmail.com>
2025-11-21 14:46:18 +0600

    feat: add MfsTransactionStatus and MfsType enums, and MfsManualTransaction model with migration

---
+++ app/Enums/MfsType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Enums;
+
+use Filament\Support\Contracts\HasColor;
+use Filament\Support\Contracts\HasIcon;
+use Filament\Support\Contracts\HasLabel;
+
+enum MfsType: string implements HasColor, HasIcon, HasLabel
+{
+    case BKASH = 'bkash';
+    case NAGAD = 'nagad';
+    case ROCKET = 'rocket';
+
+    public function getLabel(): ?string
+    {
+        return match ($this) {
+            self::BKASH => 'Bkash',
+            self::NAGAD => 'Nagad',
+            self::ROCKET => 'Rocket',
+        };
+    }
+
+    public function getColor(): string|array|null
+    {
+        return match ($this) {
+            self::BKASH => 'pink',
+            self::NAGAD => 'orange',
+            self::ROCKET => 'purple',
+        };
+    }
+
+    public function getIcon(): ?string
+    {
+        return match ($this) {
+            self::BKASH => 'heroicon-o-device-phone-mobile',
+            self::NAGAD => 'heroicon-o-device-phone-mobile',
+            self::ROCKET => 'heroicon-o-device-phone-mobile',
+        };
+    }
+}
