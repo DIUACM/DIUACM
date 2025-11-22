e07d760e43db5e17c4d6376e7e01134abc26d8d2
Sourov Biswas <sourovcodes@gmail.com>
2025-11-22 18:18:23 +0600

    feat: add policies for internal contests, registrations, payments, and manual transactions

---
+++ app/Policies/MfsManualTransactionPolicy.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Policies;
+
+use Illuminate\Foundation\Auth\User as AuthUser;
+use App\Models\MfsManualTransaction;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class MfsManualTransactionPolicy
+{
+    use HandlesAuthorization;
+    
+    public function viewAny(AuthUser $authUser): bool
+    {
+        return $authUser->can('ViewAny:MfsManualTransaction');
+    }
+
+    public function view(AuthUser $authUser, MfsManualTransaction $mfsManualTransaction): bool
+    {
+        return $authUser->can('View:MfsManualTransaction');
+    }
+
+    public function create(AuthUser $authUser): bool
+    {
+        return $authUser->can('Create:MfsManualTransaction');
+    }
+
+    public function update(AuthUser $authUser, MfsManualTransaction $mfsManualTransaction): bool
+    {
+        return $authUser->can('Update:MfsManualTransaction');
+    }
+
+    public function delete(AuthUser $authUser, MfsManualTransaction $mfsManualTransaction): bool
+    {
+        return $authUser->can('Delete:MfsManualTransaction');
+    }
+
+    public function restore(AuthUser $authUser, MfsManualTransaction $mfsManualTransaction): bool
+    {
+        return $authUser->can('Restore:MfsManualTransaction');
+    }
+
+    public function forceDelete(AuthUser $authUser, MfsManualTransaction $mfsManualTransaction): bool
+    {
+        return $authUser->can('ForceDelete:MfsManualTransaction');
+    }
+
+    public function forceDeleteAny(AuthUser $authUser): bool
+    {
+        return $authUser->can('ForceDeleteAny:MfsManualTransaction');
+    }
+
+    public function restoreAny(AuthUser $authUser): bool
+    {
+        return $authUser->can('RestoreAny:MfsManualTransaction');
+    }
+
+    public function replicate(AuthUser $authUser, MfsManualTransaction $mfsManualTransaction): bool
+    {
+        return $authUser->can('Replicate:MfsManualTransaction');
+    }
+
+    public function reorder(AuthUser $authUser): bool
+    {
+        return $authUser->can('Reorder:MfsManualTransaction');
+    }
+
+}
