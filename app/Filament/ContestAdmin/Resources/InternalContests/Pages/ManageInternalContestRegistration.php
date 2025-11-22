521d6231d943ffd4c0b6de1705dc7876329e3163
Sourov Biswas <sourovcodes@gmail.com>
2025-11-22 18:08:53 +0600

    feat: add widgets for internal contest registration stats and section chart

--- app/Filament/ContestAdmin/Resources/InternalContests/Pages/ManageInternalContestRegistration.php
+++ app/Filament/ContestAdmin/Resources/InternalContests/Pages/ManageInternalContestRegistration.php
@@ -4,6 +4,8 @@ namespace App\Filament\ContestAdmin\Resources\InternalContests\Pages;
 
 use App\Filament\ContestAdmin\Resources\InternalContests\InternalContestResource;
 use App\Filament\ContestAdmin\Resources\InternalContests\Resources\InternalContestRegistrations\InternalContestRegistrationResource;
+use App\Filament\Widgets\InternalContests\InternalContestRegistrationStats;
+use App\Filament\Widgets\InternalContests\InternalContestSectionChart;
 use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ManageRelatedRecords;
 use Filament\Tables\Table;
@@ -22,5 +24,21 @@ class ManageInternalContestRegistration extends ManageRelatedRecords
             ->headerActions([
                 CreateAction::make(),
             ]);
+    }
+
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            InternalContestSectionChart::make(['record' => $this->getOwnerRecord()]),
+            InternalContestRegistrationStats::make(['record' => $this->getOwnerRecord()]),
+        ];
+    }
+
+    public function getHeaderWidgetsColumns(): int|array
+    {
+        return [
+            'md' => 2,
+            'xl' => 3,
+        ];
     }
 }
