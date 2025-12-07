453f78af4782890be6a599d152d916dde52e25a0
Sourov Biswas <sourovcodes@gmail.com>
2025-12-07 14:33:18 +0600

    feat: remove legacy command for updating Codeforces handles and update console schedule

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::command('app:update-codeforces-ratings')->daily();
+Schedule::command('app:update-codeforces-event-stats')->everySixHours();
+Schedule::command('app:update-atcoder-event-stats')->everySixHours();
+Schedule::command('app:recalculate-ranklist-score')->everySixHours();
